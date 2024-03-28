3cf33f8b6eb0ee336f65afe3d6f5d2dd46f4ebc7
Alexander  Batyushkin <akkur.software@gmail.com>
2024-03-28 09:40:38 +0600

    Создан и настроен пустой проект unit-тестов

---
+++ tests/uuid-fourth-tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace uuidfourthtests
+{
+	TEST_CLASS(uuidfourthtests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
